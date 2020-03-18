4e37b89a55ec5d8f84a62b5e9376bfe289a83924
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-18 22:00:57 +0300

    Remove the index route

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -13,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
