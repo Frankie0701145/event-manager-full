649189db9a4b3c88c5639801a6a8c3f92e606788
Francis Njuguna <coulsorfrancois@gmail.com>
2020-03-19 12:31:24 +0300

    Add sorting

--- controllers/allEventHandler.js
+++ controllers/allEventHandler.js
@@ -2,7 +2,7 @@ let eventModel = require('../models/event');
 
 module.exports = async (req, res)=>{
     try{
-        let events = await eventModel.find().sort({'startDateTime': 'desc'});
+        let events = await eventModel.find().sort({'startDateTime': 1});
         res.status(200).send(events);
     }catch(err){
         console.error(err);
